0d2f9d4ee4b2507f916f8c7b61e355e73a12d610
gentili <thibaut.gentili@gmail.com>
2015-05-27 19:24:32 +0200

    change1

--- app/models/annonce.rb
+++ app/models/annonce.rb
@@ -1,5 +1,6 @@
 class Annonce < ActiveRecord::Base
 	belongs_to :user
-	
-
+  has_attached_file :photo, :styles => { :medium => "300x300", :thumb => "100x100" }, :default_url => "/images/:style/missing.jpg"
+  validates_attachment_content_type :photo, :content_type => /image.*/
 end
+
